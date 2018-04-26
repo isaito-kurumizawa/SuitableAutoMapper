c3575cb363658892ebb96826cd5c0774029cae45
isaito-kurumizawa <exkurumizawa@gmail.com>
2018-04-26 16:46:08 +0900

    Add files via upload

---
+++ SuitableAutoMapper/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using SuitableAutoMapper;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var suitableMapper = new SuitableMapper();
+            var model1 = new Test1();
+            var model2 = new Test2();
+            var test = suitableMapper.Map<Test1, Test2>(model1, model2);
+        }
+    }
+
+    public class Test1
+    {
+        public Test1()
+        {
+            this.hoge1 = "TEST1";
+            this.hoge2 = 1;
+            this.hoge3 = true;
+            this.hoge4 = new Hoge(){ hoge1 = "OK!"};
+        }
+        public string hoge1 { get; set; }
+        public int hoge2 { get; set; }
+        public bool hoge3 { get; set; }
+        
+        public Hoge hoge4 { get; set; }
+    }
+
+    public class Test2
+    {
+        public Test2()
+        {
+            this.hoge1 = "TEST2";
+            this.hoge2 = 5;
+            this.hoge3 = 10;
+            this.hoge4 = new Hoge();
+        }
+        public string hoge1 { get; set; }
+        public int hoge2 { get; set; }
+        public int hoge3 { get; set; }
+
+        public Hoge hoge4 { get; set; }
+    }
+    public class Hoge
+    {
+        public string hoge1 { get; set; }
+    }
+}
---
+++ SuitableAutoMapper/SuitableAutoMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SuitableAutoMapper
+{
+    class SuitableMapper
+    {
+        public SuitableMapper()
+        {
+
+        }
+        public void Map<T1, T2>(T1 src, T2 dest)
+        {
+            var srcProperies = typeof(T1).GetProperties();
+            var destProperies = typeof(T2).GetProperties();
+
+            foreach (var property in destProperies)
+            {
+                var destproperty = srcProperies.Where(
+                    (s => s.Name == property.Name && s.GetMethod.ReturnType == property.GetMethod.ReturnType))
+                    .SingleOrDefault();
+                if (destproperty != null)
+                    property.SetValue(dest, destproperty.GetValue(src));
+            }
+        }
+    }
+}
